1e0d0b78ef89fdb4791cbd18d7bb91460b6d5076
Kriti2004 <kritikiran20@gmail.com>
2023-06-16 12:48:59 +0530

    Done

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// 1. Function Defination
+function addition(a,b){
+    console.log(a+b);
+}
+
+// 2. Function calling
+addition(30,34);
+
+// 1. Function Defination
+function substraction(p,q){
+    console.log(p-q);
+}
+// 2. Finction calling
+substraction(10,15);
+
+// 1. Function Defination
+function multiplication(p,q){
+    console.log(p*q);
+}
+// 2. Finction calling
+multiplication(25,15);
+
+// 1. Function Defination
+function division(p,q){
+    console.log(p/q);
+}
+// 2. Finction calling
+division(20,35);
